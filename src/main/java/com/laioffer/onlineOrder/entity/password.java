32db6e775142e04a90e04e28d9f9ff176857a407
haoxuan <hlu224@wisc.edu>
2022-12-12 22:14:44 -0600

    update

---
+++ src/main/java/com/laioffer/onlineOrder/entity/password.java
@@ -0,0 +1,11 @@
+package com.laioffer.onlineOrder.entity;
+
+public class password {
+    private String password = "20010507lhx";
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
